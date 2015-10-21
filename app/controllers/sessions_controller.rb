b9937e61c64d13c9d8732233a6bea82f74f6006d
Troy Mullaney <troym9731@gmail.com>
2015-10-20 19:58:05 -0700

    Finish sessions and cookies functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      remember user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -13,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
